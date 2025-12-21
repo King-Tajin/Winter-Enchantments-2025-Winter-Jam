7ff639f81e8879afe2370a661fa3d9b00614f9d0
robotoloco3 <roland@shikoski.com>
2025-12-21 09:09:52 -0600

    added functionality to snowdrift

--- src/main/java/com/king_tajin/winter_enchantments/WinterEnchantments.java
+++ src/main/java/com/king_tajin/winter_enchantments/WinterEnchantments.java
@@ -1,8 +1,10 @@
 package com.king_tajin.winter_enchantments;
 
+import com.king_tajin.winter_enchantments.events.SnowdriftEnchantmentHandler;
 import com.king_tajin.winter_enchantments.init.WinterEnchantmentsVillagers;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(WinterEnchantments.MODID)
 public class WinterEnchantments {
@@ -11,6 +13,7 @@ public class WinterEnchantments {
     public WinterEnchantments(IEventBus modEventBus) {
         WinterEnchantmentsVillagers.POI_TYPES.register(modEventBus);
         WinterEnchantmentsVillagers.VILLAGER_PROFESSIONS.register(modEventBus);
+        NeoForge.EVENT_BUS.addListener(SnowdriftEnchantmentHandler::onPlayerTick);
     }
 
 }
